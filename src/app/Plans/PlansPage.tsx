cf070d2381b07e0ad944e6f6bb8b768d7fcf108e
Mike Turley <mike.turley@alum.cs.umass.edu>
2020-08-23 22:14:22 -0400

    Bump common-ui dependency!

--- src/app/Plans/PlansPage.tsx
+++ src/app/Plans/PlansPage.tsx
@@ -1,12 +1,18 @@
 import * as React from 'react';
 import { PageSection, Title } from '@patternfly/react-core';
+import { TestComponent } from '@konveyor/common-ui';
 
 const PlansPage: React.FunctionComponent = () => (
-  <PageSection>
-    <Title headingLevel="h1" size="lg">
-      Plans Page Title
-    </Title>
-  </PageSection>
+  <>
+    <PageSection>
+      <Title headingLevel="h1" size="lg">
+        Plans Page Title
+      </Title>
+    </PageSection>
+    <PageSection variant="light">
+      <TestComponent theme="secondary" />
+    </PageSection>
+  </>
 );
 
 export default PlansPage;
