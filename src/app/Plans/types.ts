165b3c1229d01654789d76786fc460cfec3b84f1
Gilles Dubreuil <gilles@redhat.com>
2020-09-08 16:49:46 +0200

    Update plan and hook data structure (#20)
    * update plan and hook data struc.
    
    * Use a common CR/metadata/metatype
    
    * Using I to prefix interface names
    
    Co-authored-by: Ian Bolton <ibolton336@gmail.com>

--- src/app/Plans/types.ts
+++ src/app/Plans/types.ts
@@ -1,81 +1,48 @@
-// TODO This is speculative until details are avail.
-
 import { IHook } from '@app/Hooks/types';
-import { IVMwareProvider, ICNVProvider } from '@app/Providers/types';
+import { ICR } from '@app/common/types';
+import { Mapping } from '@app/Mappings/types';
+import { IVMwareProvider, ICNVProvider, IHost } from '@app/Providers/types';
 
 export interface IAddPlanDisabledObjModel {
   isAddPlanDisabled: boolean;
   disabledText: string;
 }
 
-export interface IMigHooks {
-  before: boolean;
-  beforeHook: IHook;
-  after: boolean;
-  afterHook: IHook;
+export interface IPlanVM {
+  ID: string;
+  Hooks: {
+    Before: IHook;
+    After: IHook;
+  };
+  Host: IHost;
 }
 
-export interface IMigVM {
-  ID: string;
-  Hooks: IMigHooks;
-  Creds: string;
+export interface IPlanStatus {
+  Conditions: boolean;
+  ObservedGeneration: number;
+}
+
+export interface IPlan extends ICR {
+  Spec: {
+    Provider: {
+      sourceProvider: IVMwareProvider;
+      destinationProvider: ICNVProvider;
+    };
+    Map: Mapping;
+    Warm: boolean;
+    VMs: IPlanVM[];
+  };
+  Status: IPlanStatus;
 }
 
 export interface IMigration {
-  apiVersion: string;
-  kind: string;
-  metadata: {
-    name: string;
-    namespace: string;
-    creationTimestamp: string;
+  Plan: IPlan;
+  Schedule: {
+    Begin: Date;
+    End: Date;
   };
-  spec: {
-    migPlanRef: {
-      name: string;
-      namespace: string;
-    };
-    quiescePods: boolean;
-    stage: boolean;
-  };
-  status?: string;
-}
-
-export interface IMigPlan {
-  apiVersion: string;
-  kind: string;
-  metadata: {
-    name: string;
-    namespace: string;
-    creationTimestamp: string;
-  };
-  spec: {
-    srcProvider: IVMwareProvider;
-    dstProvider: ICNVProvider;
-    mapping: string;
-    warm: boolean;
-    hooks: IMigHooks;
-    migVMList: IMigVM[];
-  };
-}
-export interface IPlan {
-  MigPlan: IMigPlan;
-  Migrations: IMigration[];
-  PlanStatus: {
-    conflictErrorMsg: string;
-    finalMigrationComplete: boolean;
-    hasCanceledCondition: boolean;
-    hasCancelingCondition: boolean;
-    hasClosedCondition: boolean;
-    hasConflictCondition: boolean;
-    hasNotReadyCondition: boolean;
-    hasPODWarnCondition: boolean;
-    hasPVWarnCondition: boolean;
-    hasReadyCondition: boolean;
-    hasRunningMigrations: boolean;
-    hasSucceededMigration: boolean;
-    hasSucceededStage: boolean;
-    isPlanLocked: boolean;
-    latestType: string;
-    latestIsFailed: boolean;
+  Status: {
+    Ready: boolean;
+    StorageReady: boolean;
   };
 }
---
+++ src/app/common/types.ts
@@ -0,0 +1,23 @@
+// TODO: lib-ui candidate
+export interface IMetaTypeMeta {
+  apiVersion: string;
+  kind: string;
+}
+
+// TODO: lib-ui candidate
+export interface IMetaObjectMeta {
+  name: string;
+  namespace: string;
+  selfLink: string;
+  uid: string;
+  resourceVersion: string;
+  generation: number;
+  creationTimestamp: string; // ISO timestamp
+  annotations?: {
+    'kubectl.kubernetes.io/last-applied-configuration': string; // JSON
+  };
+}
+
+export interface ICR extends IMetaTypeMeta {
+  metadata: IMetaObjectMeta;
+}
