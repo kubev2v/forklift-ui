f1497ad0b63ed43553020d47c06f61a028f05044
Mike Turley <mike.turley@alum.cs.umass.edu>
2020-09-14 18:52:18 -0400

    Fix types

--- src/app/Mappings/components/MappingDetailView/helpers.ts
+++ src/app/Mappings/components/MappingDetailView/helpers.ts
@@ -1,12 +1,11 @@
 import { INetworkMappingItem, IStorageMappingItem, MappingType } from '@app/Mappings/types';
-import { ICNVNetwork } from '@app/Providers/types';
 import { getMappingTargetName } from '../helpers';
 
 export const groupMappingItemsByTarget = (
   mappingItems: (IStorageMappingItem | INetworkMappingItem)[],
   mappingType: MappingType
 ): (IStorageMappingItem | INetworkMappingItem)[][] => {
-  const targetNames: (ICNVNetwork | string)[] = Array.from(
+  const targetNames: string[] = Array.from(
     new Set(mappingItems.map((item) => getMappingTargetName(item.target, mappingType)))
   );
   return targetNames.map((targetName) =>
