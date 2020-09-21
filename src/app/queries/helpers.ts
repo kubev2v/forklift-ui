d1478dde2ee1bda4a87ea9661cf27a3c60d354d0
Mike Turley <mike.turley@alum.cs.umass.edu>
2020-09-21 15:38:28 -0400

    Simplify useMockableQuery

--- src/app/queries/helpers.ts
+++ src/app/queries/helpers.ts
@@ -1,48 +1,13 @@
-import {
-  QueryConfig,
-  QueryFunction,
-  QueryKey,
-  QueryResult,
-  QueryStatus,
-  useQuery,
-} from 'react-query';
+import { QueryConfig, QueryFunction, QueryKey, QueryResult, useQuery } from 'react-query';
 
 export const useMockableQuery = <TResult = unknown, TError = unknown>(
   queryKey: QueryKey,
   queryFn: QueryFunction<TResult>,
   mockData: TResult,
   queryConfig?: QueryConfig<TResult, TError> | undefined
-): QueryResult<TResult, TError> => {
-  const realQueryResult = useQuery<TResult, TError>(
+): QueryResult<TResult, TError> =>
+  useQuery<TResult, TError>(
     queryKey,
-    process.env.DATA_SOURCE !== 'mock' ? queryFn : () => (undefined as unknown) as TResult,
+    process.env.DATA_SOURCE !== 'mock' ? queryFn : () => Promise.resolve(mockData),
     queryConfig
   );
-  if (process.env.DATA_SOURCE !== 'mock') {
-    return realQueryResult;
-  } else {
-    const mockQueryResult: QueryResult<TResult, TError> = {
-      canFetchMore: undefined,
-      clear: () => undefined,
-      data: mockData,
-      error: null,
-      failureCount: 0,
-      fetchMore: () => Promise.resolve(undefined),
-      isError: false,
-      isFetched: true,
-      isFetchedAfterMount: true,
-      isFetching: false,
-      isIdle: queryConfig?.enabled === false || false,
-      isInitialData: false,
-      isLoading: false,
-      isPreviousData: false,
-      isStale: true,
-      isSuccess: true,
-      refetch: () => Promise.resolve(undefined),
-      remove: () => undefined,
-      status: QueryStatus.Success,
-      updatedAt: new Date().getTime(),
-    };
-    return mockQueryResult;
-  }
-};
