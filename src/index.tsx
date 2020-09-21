14d874d123ca56d088f458c0a772eed56f957154
Mike Turley <mike.turley@alum.cs.umass.edu>
2020-09-21 14:40:55 -0400

    Install react-query, show react-query-devtools when in dev mode

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from '@app/index';
 
+let app: React.ReactElement | null = null;
+
 if (process.env.NODE_ENV !== 'production') {
   const config = {
     rules: [
@@ -14,6 +16,17 @@ if (process.env.NODE_ENV !== 'production') {
   // eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
   const axe = require('react-axe');
   axe(React, ReactDOM, 1000, config);
+
+  // eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+  const { ReactQueryDevtools } = require('react-query-devtools');
+  app = (
+    <>
+      <App />
+      <ReactQueryDevtools />
+    </>
+  );
+} else {
+  app = <App />;
 }
 
-ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
+ReactDOM.render(app, document.getElementById('root') as HTMLElement);
