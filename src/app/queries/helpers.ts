539d876a95d0cf78c076f0ae3eac92d3f6f16cb9
Mike Turley <mike.turley@alum.cs.umass.edu>
2020-09-22 08:57:25 -0400

    Simulate 1 second loading times in mock mode

--- src/app/queries/helpers.ts
+++ src/app/queries/helpers.ts
@@ -4,13 +4,25 @@ import { UseQueryObjectConfig, QueryResult, useQuery } from 'react-query';
 // TODO what about usePaginatedQuery, useInfiniteQuery?
 // TODO what about alternate param signatures for useQuery?
 
+const mockPromise = <TResult>(data: TResult, timeout = 1000, success = true) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (success) {
+        resolve(data);
+      } else {
+        reject({ message: 'Error' });
+      }
+    }, timeout);
+  });
+};
+
 export const useMockableQuery = <TResult = unknown, TError = unknown>(
   config: UseQueryObjectConfig<TResult, TError>,
   mockData: TResult
 ): QueryResult<TResult, TError> =>
   useQuery<TResult, TError>({
     ...config,
-    queryFn: process.env.DATA_SOURCE !== 'mock' ? config.queryFn : () => Promise.resolve(mockData),
+    queryFn: process.env.DATA_SOURCE !== 'mock' ? config.queryFn : () => mockPromise(mockData),
   });
 
 export const getApiUrl = (relativePath: string): string =>
