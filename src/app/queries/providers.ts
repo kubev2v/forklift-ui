4dd6d215c2454d4bf2c31c713d498f61128cd2d4
Mike Turley <mike.turley@alum.cs.umass.edu>
2020-09-22 12:32:04 -0400

    Add polling

--- src/app/queries/providers.ts
+++ src/app/queries/providers.ts
@@ -1,4 +1,5 @@
 import { QueryResult } from 'react-query';
+import { POLLING_INTERVAL } from './constants';
 import { useMockableQuery, getApiUrl } from './helpers';
 import { MOCK_PROVIDERS } from './mocks/providers.mock';
 import { IProvidersByType } from './types';
@@ -9,6 +10,7 @@ export const useProvidersQuery = (): QueryResult<IProvidersByType> =>
     {
       queryKey: 'providers',
       queryFn: () => fetch(getApiUrl('/providers?detail=true')).then((res) => res.json()),
+      config: { refetchInterval: POLLING_INTERVAL },
     },
     MOCK_PROVIDERS
   );
