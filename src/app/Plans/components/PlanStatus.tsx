2de03119ee8667fcfc11771477bf90906284af5c
Gilles Dubreuil <gilles@redhat.com>
2020-09-22 22:24:05 +0200

    Level Plan status

--- src/app/Plans/components/PlanStatus.tsx
+++ src/app/Plans/components/PlanStatus.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { IMigration, IPlan } from '@app/queries/types';
 import { StatusIcon, StatusType } from '@konveyor/lib-ui';
-import { Progress, ProgressVariant } from '@patternfly/react-core';
+import { Level, LevelItem, Progress, ProgressMeasureLocation } from '@patternfly/react-core';
 
 interface IPlanStatusProps {
   plan: IPlan;
@@ -15,18 +15,22 @@ const PlanStatus: React.FunctionComponent<IPlanStatusProps> = ({
   if (plan.status.conditions.every((condition) => condition.type === 'Ready')) {
     return <StatusIcon status={StatusType.Ok} label="Ready" />;
   } else {
-    // TODO: This is only a placeholder more status check needs to be done once clarity with statuses.
+    // TODO: This is only a placeholder, more statuses check needs to be done.
     const totalVMs = plan.spec.vmList.length;
     const percentVMsDone = totalVMs > 0 ? (migration.status.nbVMsDone * 100) / totalVMs : 0;
     const label = 'Running';
 
     return (
       <>
-        <StatusIcon status={StatusType.Warning} label={label} />
-        <Progress
-          value={percentVMsDone}
-          label={`${migration.status.nbVMsDone} of ${plan.spec.vmList.length} VMs migrated`}
-        />
+        <Level>
+          <LevelItem>
+            <StatusIcon status={StatusType.Warning} label={label} />
+          </LevelItem>
+          <LevelItem>
+            {`${migration.status.nbVMsDone} of ${plan.spec.vmList.length} VMs migrated`}
+          </LevelItem>
+        </Level>
+        <Progress value={percentVMsDone} measureLocation={ProgressMeasureLocation.none} />
       </>
     );
   }
