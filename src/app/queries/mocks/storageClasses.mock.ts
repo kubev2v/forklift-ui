bd57f6d9c9db70b09e98c253b0be791e8b43f789
Mike Turley <mike.turley@alum.cs.umass.edu>
2020-09-27 07:42:35 -0400

    Mock tree-shaking conditions for datastores and storage classes

--- src/app/queries/mocks/storageClasses.mock.ts
+++ src/app/queries/mocks/storageClasses.mock.ts
@@ -2,37 +2,34 @@ import { IStorageClass } from '../types';
 
 export let MOCK_STORAGE_CLASSES_BY_PROVIDER: { [key: string]: IStorageClass[] } = {};
 
-// TODO put this condition back when we don't directly import mocks into components anymore
-//if (process.env.NODE_ENV === 'test' || process.env.DATA_SOURCE === 'mock') {
+if (process.env.NODE_ENV === 'test' || process.env.DATA_SOURCE === 'mock') {
+  const exampleStorageClasses: IStorageClass[] = [
+    {
+      uid: 'foo-sc-uid-1',
+      version: '1',
+      namespace: 'foo',
+      name: 'standard',
+      selfLink: '/foo',
+    },
+    {
+      uid: 'foo-sc-uid-2',
+      version: '1',
+      namespace: 'foo',
+      name: 'large',
+      selfLink: '/foo',
+    },
+    {
+      uid: 'foo-sc-uid-3',
+      version: '1',
+      namespace: 'foo',
+      name: 'small',
+      selfLink: '/foo',
+    },
+  ];
 
-const exampleStorageClasses: IStorageClass[] = [
-  {
-    uid: 'foo-sc-uid-1',
-    version: '1',
-    namespace: 'foo',
-    name: 'standard',
-    selfLink: '/foo',
-  },
-  {
-    uid: 'foo-sc-uid-2',
-    version: '1',
-    namespace: 'foo',
-    name: 'large',
-    selfLink: '/foo',
-  },
-  {
-    uid: 'foo-sc-uid-3',
-    version: '1',
-    namespace: 'foo',
-    name: 'small',
-    selfLink: '/foo',
-  },
-];
-
-MOCK_STORAGE_CLASSES_BY_PROVIDER = {
-  OCPv_1: [...exampleStorageClasses],
-  OCPv_2: [...exampleStorageClasses],
-  OCPv_3: [...exampleStorageClasses],
-};
-
-//}
+  MOCK_STORAGE_CLASSES_BY_PROVIDER = {
+    OCPv_1: [...exampleStorageClasses],
+    OCPv_2: [...exampleStorageClasses],
+    OCPv_3: [...exampleStorageClasses],
+  };
+}
