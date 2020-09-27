341a513c5e4688f98410e6a6cec30b83427ebec5
Mike Turley <mike.turley@alum.cs.umass.edu>
2020-09-27 07:02:33 -0400

    Use networks queries

--- src/app/queries/mappings.ts
+++ src/app/queries/mappings.ts
@@ -9,7 +9,7 @@ import {
 import { useStorageClassesQuery } from './storageClasses';
 import { MOCK_VMWARE_DATASTORES_BY_PROVIDER } from './mocks/datastores.mock';
 import { getAggregateQueryStatus, getFirstQueryError } from './helpers';
-import { MOCK_VMWARE_NETWORKS, MOCK_OPENSHIFT_NETWORKS } from './mocks/networks.mock';
+import { useOpenShiftNetworksQuery, useVMwareNetworksQuery } from './networks';
 
 // TODO actual useMappingsQuery bits
 
@@ -27,16 +27,16 @@ export const useMappingResourceQueries = (
   targetProvider: IOpenShiftProvider | null,
   mappingType: MappingType
 ): IMappingResourcesResult => {
-  // TODO vmware networks query
-  // TODO openshift networks query
+  const vmwareNetworksQuery = useVMwareNetworksQuery(sourceProvider);
+  const openshiftNetworksQuery = useOpenShiftNetworksQuery(targetProvider);
   // TODO vmware datastores query
   const storageClassesQuery = useStorageClassesQuery(targetProvider ? [targetProvider] : null);
 
   let availableSources: MappingSource[] = [];
   let availableTargets: MappingTarget[] = [];
   if (mappingType === MappingType.Network) {
-    availableSources = sourceProvider ? MOCK_VMWARE_NETWORKS : []; // TODO use query data
-    availableTargets = targetProvider ? MOCK_OPENSHIFT_NETWORKS : []; // TODO use query data
+    availableSources = (sourceProvider && vmwareNetworksQuery.data) || [];
+    availableTargets = (targetProvider && openshiftNetworksQuery.data) || [];
   }
   if (mappingType === MappingType.Storage) {
     availableSources = sourceProvider ? MOCK_VMWARE_DATASTORES_BY_PROVIDER.VCenter1 : []; // TODO use query data
@@ -47,8 +47,9 @@ export const useMappingResourceQueries = (
       [];
   }
 
-  const status = getAggregateQueryStatus([storageClassesQuery]); // TODO add the other queries
-  const error = getFirstQueryError([storageClassesQuery]); // TODO add the other queries
+  const allQueries = [storageClassesQuery, vmwareNetworksQuery, openshiftNetworksQuery]; // TODO add the other queries
+  const status = getAggregateQueryStatus(allQueries);
+  const error = getFirstQueryError(allQueries);
 
   return {
     availableSources,
--- src/app/queries/networks.ts
+++ src/app/queries/networks.ts
@@ -1,46 +1,46 @@
 import { usePollingContext } from '@app/common/context';
+import { ProviderType } from '@app/common/constants';
 import { QueryResult } from 'react-query';
 import { POLLING_INTERVAL } from './constants';
 import { useMockableQuery, getApiUrl, sortResultsByName } from './helpers';
 import { MOCK_OPENSHIFT_NETWORKS, MOCK_VMWARE_NETWORKS } from './mocks/networks.mock';
-import { IOpenShiftNetwork, IOpenShiftProvider, IVMwareNetwork, IVMwareProvider } from './types';
+import {
+  IOpenShiftNetwork,
+  IOpenShiftProvider,
+  IVMwareNetwork,
+  IVMwareProvider,
+  Provider,
+} from './types';
 
 // TODO handle error messages? (query.status will correctly show 'error', but error messages aren't collected)
+export const useNetworksQuery = <T extends IVMwareNetwork | IOpenShiftNetwork>(
+  provider: Provider | null,
+  providerType: ProviderType,
+  mockNetworks: T[]
+): QueryResult<T[]> => {
+  const apiSlug =
+    providerType === ProviderType.vsphere ? '/networks' : '/networkattachmentdefinitions';
+  const result = useMockableQuery<T[]>(
+    {
+      queryKey: `${providerType}-networks:${provider?.name}`,
+      queryFn: () =>
+        fetch(getApiUrl(`${provider?.selfLink || ''}${apiSlug}`)).then((res) => res.json()),
+      config: {
+        enabled: !!provider,
+        refetchInterval: usePollingContext().isPollingEnabled ? POLLING_INTERVAL : false,
+      },
+    },
+    mockNetworks
+  );
+  return sortResultsByName<T>(result);
+};
+
 export const useVMwareNetworksQuery = (
   provider: IVMwareProvider | null
-): QueryResult<IVMwareNetwork[]> => {
-  const result = useMockableQuery<IVMwareNetwork[]>(
-    {
-      queryKey: `vmware-networks:${provider?.name}`,
-      queryFn: () =>
-        fetch(getApiUrl(`${provider?.selfLink || ''}/networks`)).then((res) => res.json()),
-      config: {
-        enabled: !!provider,
-        refetchInterval: usePollingContext().isPollingEnabled ? POLLING_INTERVAL : false,
-      },
-    },
-    MOCK_VMWARE_NETWORKS
-  );
-  return sortResultsByName(result);
-};
+): QueryResult<IVMwareNetwork[]> =>
+  useNetworksQuery(provider, ProviderType.vsphere, MOCK_VMWARE_NETWORKS);
 
-// TODO handle error messages? (query.status will correctly show 'error', but error messages aren't collected)
 export const useOpenShiftNetworksQuery = (
   provider: IOpenShiftProvider | null
-): QueryResult<IOpenShiftNetwork[]> => {
-  const result = useMockableQuery<IOpenShiftNetwork[]>(
-    {
-      queryKey: `openshift-networks:${provider?.name}`,
-      queryFn: () =>
-        fetch(getApiUrl(`${provider?.selfLink || ''}/networkattachmentdefinitions`)).then((res) =>
-          res.json()
-        ),
-      config: {
-        enabled: !!provider,
-        refetchInterval: usePollingContext().isPollingEnabled ? POLLING_INTERVAL : false,
-      },
-    },
-    MOCK_OPENSHIFT_NETWORKS
-  );
-  return sortResultsByName(result);
-};
+): QueryResult<IOpenShiftNetwork[]> =>
+  useNetworksQuery(provider, ProviderType.openshift, MOCK_OPENSHIFT_NETWORKS);
