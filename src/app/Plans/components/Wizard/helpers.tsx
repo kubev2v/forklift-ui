e8313b5ed255330d71741c6dd84133db1c90f69b
Mike Turley <mike.turley@alum.cs.umass.edu>
2020-09-29 17:56:17 -0400

    Fix search logic

--- src/app/Plans/components/Wizard/helpers.tsx
+++ src/app/Plans/components/Wizard/helpers.tsx
@@ -3,6 +3,17 @@ import { TreeViewDataItem } from '@patternfly/react-core';
 import { VMwareTree } from '@app/queries/types';
 import { ClusterIcon, OutlinedHddIcon, FolderIcon } from '@patternfly/react-icons';
 
+const subtreeMatchesSearch = (node: VMwareTree, searchText: string) => {
+  if (node.kind === 'VM') return false; // Exclude VMs from the tree entirely
+  if (
+    searchText === '' ||
+    (node.object?.name || '').toLowerCase().includes(searchText.toLowerCase())
+  ) {
+    return true;
+  }
+  return node.children && node.children.some((child) => subtreeMatchesSearch(child, searchText));
+};
+
 const convertVMwareTreeNode = (node: VMwareTree, searchText: string): TreeViewDataItem => ({
   name: node.object?.name || '',
   children: filterAndConvertVMwareTreeChildren(node.children, searchText),
@@ -20,11 +31,8 @@ const filterAndConvertVMwareTreeChildren = (
   children: VMwareTree[] | null,
   searchText: string
 ): TreeViewDataItem[] | undefined => {
-  const filteredChildren = ((children || []) as VMwareTree[]).filter(
-    (node) =>
-      node.kind !== 'VM' && // Don't show VMs in the tree
-      (searchText === '' ||
-        (node.object?.name || '').toLowerCase().includes(searchText.toLowerCase()))
+  const filteredChildren = ((children || []) as VMwareTree[]).filter((node) =>
+    subtreeMatchesSearch(node, searchText)
   );
   if (filteredChildren.length > 0)
     return filteredChildren.map((node) => convertVMwareTreeNode(node, searchText));
