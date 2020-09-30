b7257bad9bfa3946033a761301065fd2e0578a15
Gilles Dubreuil <gilles@redhat.com>
2020-09-30 17:45:05 +0200

    Adjust AddToolTip props

--- src/app/common/components/AddTooltip.tsx
+++ src/app/common/components/AddTooltip.tsx
@@ -4,7 +4,6 @@ import { TooltipProps } from '@patternfly/react-core/dist/js/components/Tooltip/
 
 export interface IAddTooltipProps extends TooltipProps {
   isTooltipEnabled: boolean;
-  content: string;
   children: React.ReactElement;
 }
 
@@ -13,6 +12,6 @@ const AddTooltip: React.FunctionComponent<IAddTooltipProps> = ({
   isTooltipEnabled,
   children,
   ...props
-}) => (isTooltipEnabled ? <Tooltip {...props}>{children}</Tooltip> : children);
+}: IAddTooltipProps) => (isTooltipEnabled ? <Tooltip {...props}>{children}</Tooltip> : children);
 
 export default AddTooltip;
