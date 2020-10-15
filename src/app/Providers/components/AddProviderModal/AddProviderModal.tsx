116dfa2f1f5f9b717770d2990adeccb855dbbf47
Mike Turley <mike.turley@alum.cs.umass.edu>
2020-10-15 14:43:31 -0400

    Simplify types for form values

--- src/app/Providers/components/AddProviderModal/AddProviderModal.tsx
+++ src/app/Providers/components/AddProviderModal/AddProviderModal.tsx
@@ -12,10 +12,9 @@ import {
 import SimpleSelect, { OptionWithValue } from '@app/common/components/SimpleSelect';
 import { ProviderType, PROVIDER_TYPE_NAMES } from '@app/common/constants';
 import { usePausedPollingEffect } from '@app/common/context';
-import './AddProviderModal.css';
 import { useCreateProviderMutation } from '@app/queries';
-import { useVMwareFormState, VMwareFormState } from './useVMwareFormState';
-import { useOpenshiftFormState, OpenshiftFormState } from './useOpenshiftFormState';
+
+import './AddProviderModal.css';
 
 interface IAddProviderModalProps {
   onClose: () => void;
@@ -26,27 +25,48 @@ const PROVIDER_TYPE_OPTIONS = Object.values(ProviderType).map((type) => ({
   value: type,
 })) as OptionWithValue<ProviderType>[];
 
+const useAddProviderFormState = () => {
+  // TODO determine the actual validation criteria for this form -- these are for testing
+  const providerTypeField = useFormField<ProviderType | null>(
+    null,
+    yup.mixed().label('Provider type').oneOf(Object.values(ProviderType)).required()
+  );
+  return {
+    [ProviderType.vsphere]: useFormState({
+      providerType: providerTypeField,
+      name: useFormField('', yup.string().label('Name').min(2).max(20).required()),
+      hostname: useFormField('', yup.string().label('Hostname').max(40).required()),
+      username: useFormField('', yup.string().label('Username').max(20).required()),
+      password: useFormField('', yup.string().label('Password').max(20).required()),
+    }),
+    [ProviderType.openshift]: useFormState({
+      providerType: providerTypeField,
+      clusterName: useFormField('', yup.string().label('Cluster name').max(40).required()),
+      url: useFormField('', yup.string().label('URL').max(40).required()),
+      saToken: useFormField('', yup.string().label('Service account token').max(20).required()),
+    }),
+  };
+};
+
+type AddProviderFormState = ReturnType<typeof useAddProviderFormState>; // ✨ Magic
+export type VMwareProviderFormValues = AddProviderFormState[ProviderType.vsphere]['values'];
+export type OpenshiftProviderFormValues = AddProviderFormState[ProviderType.openshift]['values'];
+export type AddProviderFormValues = VMwareProviderFormValues | OpenshiftProviderFormValues;
+
 const AddProviderModal: React.FunctionComponent<IAddProviderModalProps> = ({
   onClose,
 }: IAddProviderModalProps) => {
   usePausedPollingEffect();
 
-  const providerTypeField = useFormField<ProviderType | null>(
-    null,
-    yup.mixed().label('Provider type').oneOf(Object.values(ProviderType)).required()
-  );
-  const vmwareForm = useVMwareFormState(providerTypeField);
-  const openshiftForm = useOpenshiftFormState(providerTypeField);
-
-  // TODO determine the actual validation criteria for this form -- these are for testing
-
+  const forms = useAddProviderFormState();
+  const vmwareForm = forms[ProviderType.vsphere];
+  const openshiftForm = forms[ProviderType.openshift];
+  const providerTypeField = vmwareForm.fields.providerType;
   const providerType = providerTypeField.value;
-  const formValues =
-    providerType === ProviderType.vsphere ? vmwareForm.values : openshiftForm.values;
-  const isFormValid =
-    providerType === ProviderType.vsphere ? vmwareForm.isValid : openshiftForm.isValid;
+  const formValues = providerType ? forms[providerType].values : vmwareForm.values;
+  const isFormValid = providerType ? forms[providerType].isValid : false;
 
-  const [createProvider, createProviderResult] = useCreateProviderMutation(() => onClose());
+  const [createProvider, createProviderResult] = useCreateProviderMutation(providerType, onClose);
   // TODO render loading/error/result from returned createProviderResult
 
   return (
@@ -80,7 +100,7 @@ const AddProviderModal: React.FunctionComponent<IAddProviderModalProps> = ({
           isRequired
           fieldId="provider-type"
           className={!providerType ? 'extraSelectMargin' : ''}
-          {...getFormGroupProps(vmwareForm.fields.providerType)}
+          {...getFormGroupProps(providerTypeField)}
         >
           <SimpleSelect
             id="provider-type"
