bdb50d178273115559e969862cf881d773e64570
Mike Turley <mike.turley@alum.cs.umass.edu>
2020-11-16 09:41:28 -0500

    Ignore self-signed certs in remote dev mode (#229)

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -3,6 +3,7 @@ _dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $_dir/..
 export NODE_ENV=development
 export EXPRESS_PORT=9001
+export NODE_TLS_REJECT_UNAUTHORIZED="0"
 yarn concurrently --names "EXPRESS,WEBPACK" -c "green.bold.inverse,blue.bold.inverse" \
   "$_dir/run-local-express.sh --auto-reload" \
   "./node_modules/webpack-dev-server/bin/webpack-dev-server.js \
