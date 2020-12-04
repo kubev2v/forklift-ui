79ea6fec5b05fa0d8de6e13d4b0d171222cfd06e
Mike Turley <mike.turley@alum.cs.umass.edu>
2020-12-04 11:56:07 -0500

    Add $(npm bin) to all scripts depending on local packages

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -5,7 +5,7 @@ export NODE_ENV=development
 export DEBUG=1
 export EXPRESS_PORT=9001
 export NODE_TLS_REJECT_UNAUTHORIZED="0"
-npm run concurrently --names "EXPRESS,WEBPACK" -c "green.bold.inverse,blue.bold.inverse" \
+$(npm bin)/concurrently --names "EXPRESS,WEBPACK" -c "green.bold.inverse,blue.bold.inverse" \
   "$_dir/run-local-express.sh --auto-reload" \
-  "./node_modules/webpack-dev-server/bin/webpack-dev-server.js \
+  "$(npm bin)/webpack-dev-server \
     --hot --color --progress --info=true --config=$_dir/../config/webpack.dev.js"
