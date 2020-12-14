6022ad0d6b55a4190aaf061dcb9c732aa2af4525
Gilles Dubreuil <gilles@redhat.com>
2020-12-14 17:29:48 +0100

    Unit tests for OpenShiftProvidersTable

---
+++ src/app/Providers/components/ProvidersTable/OpenShift/__tests__/OpenShiftProvidersTable.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
+import '@testing-library/jest-dom';
+import { Router } from 'react-router-dom';
+
+import { NetworkContextProvider } from '@app/common/context';
+import OpenShiftProvidersTable from '../OpenShiftProvidersTable';
+import { MOCK_PROVIDERS } from '@app/queries/mocks/providers.mock';
+
+describe('<OpenShiftProvidersTable />', () => {
+  const history = createMemoryHistory();
+  const props = {
+    providers: MOCK_PROVIDERS.openshift,
+  };
+
+  it('renders openshift table', () => {
+    render(
+      <NetworkContextProvider>
+        <Router history={history}>
+          <OpenShiftProvidersTable {...props} />
+        </Router>
+      </NetworkContextProvider>
+    );
+
+    expect(
+      screen.getByRole('grid', { name: /OpenShift Virtualization providers table/ })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('row', { name: /ocpv-1 https:\/\/my_OCPv_url 41 26 8 0 Ready Actions/ })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('row', { name: /ocpv-2 https:\/\/my_OCPv_url 41 26 8 0 Critical Actions/ })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('row', { name: /ocpv-3 https:\/\/my_OCPv_url 41 26 8 0 Ready Actions/ })
+    ).toBeInTheDocument();
+  });
+
+  // The expanding section doesn't render, is it the right button?
+  it.skip('renders storage classes', async () => {
+    render(
+      <NetworkContextProvider>
+        <Router history={history}>
+          <OpenShiftProvidersTable {...props} />
+        </Router>
+      </NetworkContextProvider>
+    );
+
+    const sharedClasses = screen.getAllByRole('button', { name: '0' });
+    userEvent.click(sharedClasses[2]);
+
+    await waitFor(() => {
+      expect(screen.getByText('large')).toBeInTheDocument();
+    });
+  });
+
+  it('renders status condition', async () => {
+    render(
+      <NetworkContextProvider>
+        <Router history={history}>
+          <OpenShiftProvidersTable {...props} />
+        </Router>
+      </NetworkContextProvider>
+    );
+
+    userEvent.click(screen.getByRole('button', { name: /Critical/ }));
+
+    await waitFor(() => {
+      expect(screen.getByText('The provider is not responding.')).toBeInTheDocument();
+    });
+  });
+
+  it('renders action menu', async () => {
+    render(
+      <NetworkContextProvider>
+        <Router history={history}>
+          <OpenShiftProvidersTable {...props} />
+        </Router>
+      </NetworkContextProvider>
+    );
+
+    userEvent.click(screen.getAllByRole('button', { name: /Actions/ })[0]);
+
+    await waitFor(() => {
+      const dropdownEdit = screen.getByRole('menuitem', { name: /Edit/ });
+      const actionEdit = screen.getByText(/Edit/);
+
+      expect(dropdownEdit).toHaveTextContent('Edit');
+      expect(dropdownEdit).toContainElement(actionEdit);
+
+      const dropdownRemove = screen.getByRole('menuitem', { name: /Remove/ });
+      const actionRemove = screen.getByText(/Remove/);
+
+      expect(dropdownRemove).toHaveTextContent('Remove');
+      expect(dropdownRemove).toContainElement(actionRemove);
+    });
+  });
+});
