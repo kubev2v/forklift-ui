79ca6fbc22e070825e6c87b5af453b4131d2279f
Gilles Dubreuil <gilles@redhat.com>
2020-12-17 18:43:30 +0100

    Adds navigation for provider types (#343)
    * Add navigation for provider types
    
    * Fix tests
    
    * Use better selector for welcome heading
    
    * useHistory() instead of useState() for current provider tab
    
    * Get welcome heading with role's name
    
    * Restore useEffect to select initial tab when on base /providers route
    
    * Use APP_TITLE in test instead of hard-coding Forklift
    
    Co-authored-by: Mike Turley <mike.turley@alum.cs.umass.edu>

--- src/app/app.test.tsx
+++ src/app/app.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render, waitFor, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { App } from '.';
 import { APP_TITLE } from './common/constants';
@@ -7,7 +7,6 @@ import { APP_TITLE } from './common/constants';
 describe('App', () => {
   test('renders welcome page without errors', async () => {
     render(<App />);
-    await waitFor(() => screen.getByRole('heading'));
-    expect(screen.getByRole('heading')).toHaveTextContent(APP_TITLE);
+    expect(screen.getByRole('heading', { name: new RegExp(APP_TITLE) })).toBeInTheDocument;
   });
 });
