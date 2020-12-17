d8f0d9f96ee19ead1a86c2e46d73085e8f8d7dc6
Mike Turley <mike.turley@alum.cs.umass.edu>
2020-12-17 12:40:09 -0500

    Use APP_TITLE in test instead of hard-coding Forklift

--- src/app/app.test.tsx
+++ src/app/app.test.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { App } from '.';
+import { APP_TITLE } from './common/constants';
 
 describe('App', () => {
   test('renders welcome page without errors', async () => {
     render(<App />);
-    expect(screen.getByRole('heading', { name: /Forklift/ })).toBeInTheDocument;
+    expect(screen.getByRole('heading', { name: new RegExp(APP_TITLE) })).toBeInTheDocument;
   });
 });
