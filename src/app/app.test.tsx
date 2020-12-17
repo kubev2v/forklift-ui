efe0d30c5f9a172bdf7f3a8f1f052443d729c0f1
Gilles Dubreuil <gilles@redhat.com>
2020-12-17 18:10:22 +0100

    Get welcome heading with role's name

--- src/app/app.test.tsx
+++ src/app/app.test.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
-import { render, waitFor, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { App } from '.';
-import { APP_TITLE } from './common/constants';
 
 describe('App', () => {
   test('renders welcome page without errors', async () => {
     render(<App />);
-    const pattern = `Welcome to ${APP_TITLE}`;
-    const welcome = new RegExp(pattern);
-    expect(screen.getByRole('heading', { name: welcome })).toBeInTheDocument;
+    expect(screen.getByRole('heading', { name: /Forklift/ })).toBeInTheDocument;
   });
 });
