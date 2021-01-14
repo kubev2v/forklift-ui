c137d587a217d08284c19691ad3abc57c7fca2b2
Mike Turley <mike.turley@alum.cs.umass.edu>
2021-01-14 12:58:12 -0500

    Add qe-tests folder with qe-committers as CODEOWNERS, add to eslint, prettier and tsc configs (#380)

---
+++ qe-tests/README.md
@@ -0,0 +1,3 @@
+# End-to-end Cypress tests for forklift-ui
+
+TODO: this is a placeholder
---
+++ qe-tests/hello.ts
@@ -0,0 +1,2 @@
+// TODO: this is a placeholder, just so eslint, tsc and prettier have something to detect here
+console.log('Hello World');
