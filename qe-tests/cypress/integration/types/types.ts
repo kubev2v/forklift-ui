aec6c984221cbdf2856f1e0023e9d8dad4f3d7e3
nachandr <nachandr@redhat.com>
2021-03-24 18:00:52 -0500

    [RFR] Automate test to migrate to external OCP cluster (#445)
    * Automate test to migrate to external OCP cluster
    
    * Automate test to migrate to external OCP cluster
    
    * Automate test to migrate to external OCP cluster
    
    * Automate test to migrate to external OCP cluster
    
    * Automate test to migrate to external OCP cluster

--- qe-tests/cypress/integration/types/types.ts
+++ qe-tests/cypress/integration/types/types.ts
@@ -45,6 +45,7 @@ export type PlanData = {
   useExistingNetworkMapping: boolean;
   useExistingStorageMapping: boolean;
   providerData: ProviderData;
+  targetProvider?: ProviderData;
   networkMappingData: MappingData;
   storageMappingData: MappingData;
 };
