06768d4269bd9cbf3ef62be1009fb14fe906ae50
Mike Turley <mike.turley@alum.cs.umass.edu>
2021-05-04 17:20:19 -0400

    Simplify empty state

--- src/app/Plans/components/Wizard/HooksForm.tsx
+++ src/app/Plans/components/Wizard/HooksForm.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
-import TableEmptyState from '@app/common/components/TableEmptyState';
 import { PlusCircleIcon } from '@patternfly/react-icons';
 import { PlanWizardFormState } from './PlanWizard';
-import { TextContent, Text, Button } from '@patternfly/react-core';
+import {
+  TextContent,
+  Text,
+  Button,
+  EmptyState,
+  EmptyStateBody,
+  EmptyStateIcon,
+} from '@patternfly/react-core';
 import spacing from '@patternfly/react-styles/css/utilities/Spacing/spacing';
 
 /// TODO replace this!
@@ -17,29 +23,32 @@ interface IHooksFormProps {
 const HooksForm: React.FunctionComponent<IHooksFormProps> = ({ form }: IHooksFormProps) => {
   const hooks: IHook[] = []; // TODO load from a query based on what's in the plan? or prefilled form state?
 
-  const table =
-    hooks.length === 0 ? (
-      <TableEmptyState
-        icon={PlusCircleIcon}
-        bodyText="No hooks have been added to this migration plan"
-      />
-    ) : (
-      <>
-        <TextContent className={spacing.mbMd}>
-          <Text component="p">
-            Hooks are contained in Ansible playbooks that can be run before or after the migration.
-          </Text>
-        </TextContent>
-        <h1>TODO</h1>
-      </>
-    );
-
   return (
     <>
-      <Button variant="secondary" onClick={() => alert('TODO')}>
-        Add hook
-      </Button>
-      {table}
+      <TextContent className={spacing.mbMd}>
+        <Text component="p">
+          Hooks are contained in Ansible playbooks that can be run before or after the migration.
+          They are run on either the source or target provider.
+        </Text>
+      </TextContent>
+      {hooks.length === 0 ? (
+        <EmptyState className={spacing.my_2xl}>
+          <EmptyStateIcon icon={PlusCircleIcon} />
+          <EmptyStateBody className={spacing.mt_0}>
+            No hooks have been added to this migration plan
+          </EmptyStateBody>
+          <Button variant="secondary" className={spacing.mtMd} onClick={() => alert('TODO')}>
+            Add hook
+          </Button>
+        </EmptyState>
+      ) : (
+        <>
+          <Button variant="secondary" onClick={() => alert('TODO')}>
+            Add hook
+          </Button>
+          <h1>TODO: table here</h1>
+        </>
+      )}
     </>
   );
 };
