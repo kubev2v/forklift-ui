be29f2daa730be3a87fac10a5309e4e7bbe2012c
Mike Turley <mike.turley@alum.cs.umass.edu>
2021-05-05 15:12:36 -0400

    Some minor tweaks

--- src/app/Plans/components/Wizard/HooksForm.tsx
+++ src/app/Plans/components/Wizard/HooksForm.tsx
@@ -22,13 +22,16 @@ interface IHooksFormProps {
 
 const HooksForm: React.FunctionComponent<IHooksFormProps> = ({ form }: IHooksFormProps) => {
   const hooks: IHook[] = []; // TODO load from a query based on what's in the plan? or prefilled form state?
+  // TODO handle the fact that we'll be listing hook CRs that don't exist yet? I guess we can just store the whole object in form state?
+
+  const [isAddEditModalOpen, toggleAddEditModal] = React.useReducer((isOpen) => !isOpen, false);
+  // TODO show the AddEditHookModal from Gilles' PR with a isWizardMode flag or something, to add the extra fields?
 
   return (
     <>
       <TextContent className={spacing.mbMd}>
         <Text component="p">
           Hooks are contained in Ansible playbooks that can be run before or after the migration.
-          They are run on either the source or target provider.
         </Text>
       </TextContent>
       {hooks.length === 0 ? (
@@ -37,13 +40,13 @@ const HooksForm: React.FunctionComponent<IHooksFormProps> = ({ form }: IHooksFor
           <EmptyStateBody className={spacing.mt_0}>
             No hooks have been added to this migration plan
           </EmptyStateBody>
-          <Button variant="secondary" className={spacing.mtMd} onClick={() => alert('TODO')}>
+          <Button variant="secondary" className={spacing.mtMd} onClick={toggleAddEditModal}>
             Add hook
           </Button>
         </EmptyState>
       ) : (
         <>
-          <Button variant="secondary" onClick={() => alert('TODO')}>
+          <Button variant="secondary" onClick={toggleAddEditModal}>
             Add hook
           </Button>
           <h1>TODO: table here</h1>
