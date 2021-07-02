d2196efb4614e9cdec021575db55a90d86b66e3f
Fabien Dupont <fdupont@redhat.com>
2021-07-02 18:47:26 +0200

    Bug 1977657 - Put CA bundle in a writeable folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 
 # Add service serving CA cert to the global CA bundle
 if [ -f "/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt" ]; then
-    cat /var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt >> /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+    cp /var/run/secrets/kubernetes.io/serviceaccount/ca.crt /opt/app-root/src/ca.crt
+    cat /var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt >> /opt-app-root/src/ca.crt
 fi
 
 exec npm run -s start
