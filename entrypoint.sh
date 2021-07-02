e603920a043342957f5c1e20c0bf893aa2042a82
Fabien Dupont <fdupont@redhat.com>
2021-07-02 19:18:43 +0200

    Bug 1977657 - Fix typo in CA bundle file path (#682)

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # Add service serving CA cert to the global CA bundle
 if [ -f "/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt" ]; then
     cp /var/run/secrets/kubernetes.io/serviceaccount/ca.crt /opt/app-root/src/ca.crt
-    cat /var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt >> /opt-app-root/src/ca.crt
+    cat /var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt >> /opt/app-root/src/ca.crt
 fi
 
 exec npm run -s start
