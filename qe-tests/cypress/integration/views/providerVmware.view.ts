270e304a26edc156edbc76aeb994126422eec362
Mike Turley <mike.turley@alum.cs.umass.edu>
2021-07-13 07:58:16 -0400

    Aligned tests with changes in MTV 2.1, fixed VMWare provider, mappings and plan creation (#691) (#696)
    Co-authored-by: Igor Braginsky <ibragins@redhat.com>

--- qe-tests/cypress/integration/views/providerVmware.view.ts
+++ qe-tests/cypress/integration/views/providerVmware.view.ts
@@ -1,8 +1,8 @@
-export const instanceName = '#vmware-name';
-export const instanceHostname = '#vmware-hostname';
-export const instanceUsername = '#vmware-username';
-export const instancePassword = '#vmware-password';
-export const instanceFingerprint = '#vmware-fingerprint';
+export const instanceName = '#name';
+export const instanceHostname = '#hostname';
+export const instanceUsername = '#username';
+export const instancePassword = '#password';
+export const instanceFingerprint = '#fingerprint';
 export const addButtonModal =
   '#pf-modal-part-0 > footer > div > div > button.pf-c-button.pf-m-primary';
 export const vmwareMenu = '.pf-c-tabs__link';
