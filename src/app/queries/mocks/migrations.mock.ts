ee30a3e2e7aee65d2928b3c711c72f4241ccc7eb
Mike Turley <mike.turley@alum.cs.umass.edu>
2021-10-05 17:05:23 -0400

    Add a mock plan with a scheduled cutover

--- src/app/queries/mocks/migrations.mock.ts
+++ src/app/queries/mocks/migrations.mock.ts
@@ -6,7 +6,7 @@ import { MOCK_PLANS } from './plans.mock';
 export let MOCK_MIGRATIONS: IMigration[];
 
 if (process.env.NODE_ENV === 'test' || process.env.DATA_SOURCE === 'mock') {
-  const runningPlanIndexes = [0, 2, 3, 4, 6, 7, 8, 9];
+  const runningPlanIndexes = [0, 2, 3, 4, 6, 7, 8, 9, 10];
   MOCK_MIGRATIONS = runningPlanIndexes.map((index) => ({
     apiVersion: CLUSTER_API_VERSION,
     kind: 'Migration',
@@ -23,7 +23,8 @@ if (process.env.NODE_ENV === 'test' || process.env.DATA_SOURCE === 'mock') {
   // Plans with canceled VMs:
   //   plantest-03 (plan index 2, running plan index 1)
   //   plantest-07 (plan index 6, running plan index 4)
-  //   plantest-10 (plan index 9, running plan index 7)
+  //   plantest-07 (plan index 7, running plan index 5)
+  //   plantest-11 (plan index 10, running plan index 8)
   const cancelSpec = [
     {
       id: 'vm-1630',
@@ -32,8 +33,14 @@ if (process.env.NODE_ENV === 'test' || process.env.DATA_SOURCE === 'mock') {
   ];
   MOCK_MIGRATIONS[1].spec.cancel = cancelSpec;
   MOCK_MIGRATIONS[4].spec.cancel = cancelSpec;
-  MOCK_MIGRATIONS[7].spec.cancel = cancelSpec;
+  MOCK_MIGRATIONS[5].spec.cancel = cancelSpec;
+  MOCK_MIGRATIONS[8].spec.cancel = cancelSpec;
 
-  // plantest-8 (plan index 7, running plan index 5) is in cutover
-  MOCK_MIGRATIONS[5].spec.cutover = '2021-03-16T18:31:30Z';
+  // plantest-8 (plan index 7, running plan index 5) has a scheduled cutover
+  MOCK_MIGRATIONS[5].spec.cutover = new Date(
+    new Date().getTime() + 36 * 60 * 60 * 1000
+  ).toISOString();
+
+  // plantest-9 (plan index 8, running plan index 6) is in cutover
+  MOCK_MIGRATIONS[6].spec.cutover = '2021-03-16T18:31:30Z';
 }
