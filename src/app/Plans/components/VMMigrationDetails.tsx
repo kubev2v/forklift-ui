be6056b1752ebd4e8c8d76cbe8348ccac52bc766
Mike Turley <mike.turley@alum.cs.umass.edu>
2021-10-27 15:50:29 -0400

    [Backport] Add fallbacks for displaying VM name when VM is deleted from inventory (#822)
    * Add VM names to status object in TS types and mock data
    
    * Use inventory first and then status for VM names on the migration page
    
    * Use inventory first and then status for VM names everywhere

--- src/app/Plans/components/VMMigrationDetails.tsx
+++ src/app/Plans/components/VMMigrationDetails.tsx
@@ -89,19 +89,15 @@ const VMMigrationDetails: React.FunctionComponent = () => {
   const plansQuery = usePlansQuery();
   const plan = plansQuery.data?.items.find((item) => item.metadata.name === match?.params.planName);
   const planStarted = !!plan?.status?.migration?.started;
-  const vmStatuses = planStarted
-    ? plan?.status?.migration?.vms || []
-    : plan?.spec.vms.map(({ id }) => ({
-        id,
-        pipeline: [],
-        phase: '',
-      })) || [];
 
   const providersQuery = useInventoryProvidersQuery();
   const { sourceProvider } = findProvidersByRefs(plan?.spec.provider || null, providersQuery);
 
   const vmsQuery = useSourceVMsQuery(sourceProvider);
-  const getVMName = (vmStatus: IVMStatus) => vmsQuery.data?.vmsById[vmStatus.id]?.name || '';
+  const getVMName = (vmStatus: IVMStatus) => {
+    const nameFromInventory = vmsQuery.data?.vmsById[vmStatus.id]?.name || null;
+    return nameFromInventory || vmStatus.name;
+  };
 
   const migrationsQuery = useMigrationsQuery();
   const latestMigration = findLatestMigration(plan || null, migrationsQuery.data?.items || null);
@@ -113,6 +109,15 @@ const VMMigrationDetails: React.FunctionComponent = () => {
       planState === 'Copying-CutoverScheduled' ||
       planState === 'Copying-Canceled' ||
       planState === 'Copying-Failed');
+
+  const vmStatuses: IVMStatus[] = planStarted
+    ? plan?.status?.migration?.vms || []
+    : plan?.spec.vms.map(({ id }) => ({
+        id,
+        name: vmsQuery.data?.vmsById[id]?.name || '',
+        pipeline: [],
+        phase: '',
+      })) || [];
 
   const getSortValues = (vmStatus: IVMStatus) => {
     return [
@@ -216,7 +221,7 @@ const VMMigrationDetails: React.FunctionComponent = () => {
     !!(latestMigration?.spec.cancel || []).find((canceledVM) => canceledVM.id === vm.id);
   const cancelableVMs = !hasCondition(plan?.status?.conditions || [], 'Executing')
     ? []
-    : (vmStatuses as IVMStatus[]).filter((vm) => !vm.completed && !isVMCanceled(vm));
+    : vmStatuses.filter((vm) => !vm.completed && !isVMCanceled(vm));
   const selectAllCancelable = (isSelected: boolean) =>
     isSelected ? setSelectedItems(cancelableVMs) : setSelectedItems([]);
 
@@ -283,11 +288,12 @@ const VMMigrationDetails: React.FunctionComponent = () => {
       isOpen: planStarted ? isExpanded : undefined,
       cells: [
         {
-          title: vm && (
+          title: (
             <VMNameWithPowerState
               vm={vm}
+              vmStatus={vmStatus}
               sourceProvider={sourceProvider}
-              key={`row${rows.length}-${vm.name}`} // Ensure it always re-mounts when table rows change so tooltip state doesn't get messed up
+              key={`row${rows.length}-${vmStatus.id}`} // Ensure it always re-mounts when table rows change so tooltip state doesn't get messed up
             />
           ),
         },
