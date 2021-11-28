17181c7b8c0b2d7cfffe4ff1902ff494f49b406b
Gilles Dubreuil <gilles@redhat.com>
2021-11-28 16:31:46 +0100

    Sync packages; Fix path in run-local-express.sh

--- scripts/run-local-express.sh
+++ scripts/run-local-express.sh
@@ -5,7 +5,7 @@ export STATIC_DIR="$_dir/../dist"
 mkdir -p "$STATIC_DIR"
 cd $_dir/..
 if [ "$1" == "--auto-reload" ]; then
-  node --trace-deprecation $_dir/../node_modules/nodemon/bin/nodemon.js --watch $_dir/../packages/web/src $_dir/../dist/server.js
+  node --trace-deprecation $_dir/../node_modules/nodemon/bin/nodemon.js --watch $_dir/../dist $_dir/../dist/server.js
 else
   node --trace-deprecation $_dir/../dist/server.js
 fi
