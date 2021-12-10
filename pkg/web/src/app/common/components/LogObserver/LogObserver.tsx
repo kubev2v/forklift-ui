c749228c4d3c09eaddded5031809fabcc5b123bc
Michael Spaxman <seanforyou23@gmail.com>
2021-12-10 12:20:23 -0500

    [cleanup]

--- pkg/web/src/app/common/components/LogObserver/LogObserver.tsx
+++ pkg/web/src/app/common/components/LogObserver/LogObserver.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {
+  Card,
   PageSection,
-  Title,
   Toolbar,
   ToolbarItem,
   ToolbarContent,
@@ -12,16 +12,10 @@ import {
 import { useQueryClient } from 'react-query';
 import { LogViewer, LogViewerSearch } from '@patternfly/react-log-viewer';
 
-import {
-  // usePodLogsQuery,
-  useClusterPodsQuery,
-  useClusterPodLogsQuery,
-} from '@app/queries/pods';
-import { MOCK_LOGS } from '@app/queries/mocks/logs.mock';
+import { useClusterPodsQuery, useClusterPodLogsQuery } from '@app/queries/pods';
+
 import CaretDownIcon from '@patternfly/react-icons/dist/esm/icons/caret-down-icon';
 import { ContainerType, IPodObject } from '@app/queries/types';
-
-// interface ILogObserverProps {}
 
 export const LogObserver: React.FunctionComponent = () => {
   const queryClient = useQueryClient();
@@ -30,7 +24,6 @@ export const LogObserver: React.FunctionComponent = () => {
   const [logData, setLogData] = React.useState<string | undefined>(undefined);
 
   const podsQuery = useClusterPodsQuery();
-  // containerTypeSelection
 
   const [availablePods, setAvailablePods] = React.useState<IPodObject[] | undefined>();
 
@@ -38,22 +31,14 @@ export const LogObserver: React.FunctionComponent = () => {
     <DropdownItem key={pod.metadata.name}>{pod.metadata.name}</DropdownItem>
   ));
 
-  // console.log('podDropDownItems', podDropDownItems);
-
-  // console.log(availablePods)
-
-  const controllerPodName = availablePods
-    ?.map((pod) => pod.metadata.name)
-    .find((pod) => pod.includes('controller'));
   const [podSelection, setPodSelection] = React.useState<string | undefined>();
 
   React.useEffect(() => {
     setAvailablePods(podsQuery.data?.items);
     if (!podSelection) {
-      // setPodSelection(podsQuery.data?.items.map(pod => pod.metadata.name).find(pod => pod.includes('controller')))
       setPodSelection(podsQuery.data?.items[0].metadata.name);
     }
-  }, [podsQuery.data]);
+  }, [podsQuery.data, podSelection]);
 
   const [containerTypeDropDownItems, setContainerTypeDropDownItems] = React.useState<
     JSX.Element[] | undefined
@@ -64,29 +49,15 @@ export const LogObserver: React.FunctionComponent = () => {
       .find((pod) => pod.metadata.name === podSelection)
       ?.spec.containers.map((container) => container.name)
       .map((cont) => <DropdownItem key={cont}>{cont}</DropdownItem>);
-    // console.log('containers for selected pod: ', containersForSelectedPod?.map(cont => cont.key));
+
     setContainerTypeDropDownItems(containersForSelectedPod);
     const defaultContainerSelection = containersForSelectedPod
       ?.map((cont) => cont.key)[0]
       ?.toString();
     setContainerTypeSelection(defaultContainerSelection);
-    queryClient.invalidateQueries(['pod-logs', podSelection]);
-  }, [podSelection]);
+  }, [podSelection, podsQuery.data?.items]);
 
-  // console.log('Cluster pods:', podsQuery.data?.items);
-  const podLogs = useClusterPodLogsQuery(containerTypeSelection, podSelection); // standard query
-  // const podLogs = usePodLogsQuery(containerType); // mutate query
-
-  // React.useEffect(() => {
-  //   podLogs.mutate({});
-  // }, []);
-
-  // React.useEffect(() => {
-  //   setLogData(logs.data);
-  // }, [logs.data]);
-
-  // logs.data && logs.data.text().then(result => setLogData(result))
-  // console.log('podLogs', podLogs.data);
+  const podLogs = useClusterPodLogsQuery(containerTypeSelection, podSelection);
 
   React.useEffect(() => {
     podLogs.data && podLogs.data.text().then((result) => setLogData(result));
@@ -101,82 +72,97 @@ export const LogObserver: React.FunctionComponent = () => {
     false
   );
 
-  const onFocus = () => {
+  const onPodSelectionFocus = () => {
+    const element = document.getElementById('pod-toggle-id');
+    element?.focus();
+  };
+
+  const onContainerTypeSelectionFocus = () => {
     const element = document.getElementById('container-type-toggle-id');
     element?.focus();
   };
 
   const onContainerTypeSelect = (event?: React.SyntheticEvent<HTMLDivElement>) => {
     toggleIsContainerTypeDropdownOpen();
-    // console.log('event', event?.currentTarget.innerText);
     event?.currentTarget.innerText && setContainerTypeSelection(event?.currentTarget.innerText);
-    onFocus();
+    onContainerTypeSelectionFocus();
   };
 
   const onPodSelect = (event?: React.SyntheticEvent<HTMLDivElement>) => {
     toggleIsPodSelectionDropdownOpen();
-    console.log('event', event?.currentTarget.innerText);
     event?.currentTarget.innerText && setPodSelection(event?.currentTarget.innerText);
-    // onFocus();
+
+    const containersForPod = availablePods
+      ?.find((pod) => pod.metadata.name === event?.currentTarget.innerText)
+      ?.spec.containers.map((container) => container.name)
+      .map((cont) => <DropdownItem key={cont}>{cont}</DropdownItem>);
+
+    containersForPod && setContainerTypeSelection(containersForPod[0].key?.toString());
+    onPodSelectionFocus();
   };
 
-  console.log('podSelection', podSelection);
-
   return (
-    <>
-      {/* <div>{availablePods?.map(el => el.metadata.name).join(', ')}</div> */}
-      {/* <div>{controllerPodName}</div> */}
-      {podLogs.isLoading && <div>loading...</div>}
-      <LogViewer
-        loadingContent={<div>loading</div>}
-        toolbar={
-          <Toolbar>
-            <ToolbarContent>
-              <ToolbarItem>
-                {podDropDownItems && (
+    <PageSection>
+      <Card>
+        <LogViewer
+          loadingContent={
+            podsQuery.isLoading ? (
+              <div>loading pods...</div>
+            ) : podLogs.isLoading ? (
+              <div>loading logs...</div>
+            ) : (
+              <div>loading...</div>
+            )
+          }
+          toolbar={
+            <Toolbar>
+              <ToolbarContent>
+                <ToolbarItem>
+                  {podDropDownItems && (
+                    <Dropdown
+                      onSelect={onPodSelect}
+                      isOpen={isPodSelectionDropdownOpen}
+                      toggle={
+                        <DropdownToggle
+                          id="pod-toggle-id"
+                          onToggle={toggleIsPodSelectionDropdownOpen}
+                          toggleIndicator={CaretDownIcon}
+                        >
+                          {podSelection}
+                        </DropdownToggle>
+                      }
+                      dropdownItems={podDropDownItems}
+                    />
+                  )}
+                </ToolbarItem>
+                <ToolbarItem>
                   <Dropdown
-                    onSelect={onPodSelect}
-                    isOpen={isPodSelectionDropdownOpen}
+                    onSelect={onContainerTypeSelect}
+                    isOpen={isContainerTypeDropdownOpen}
                     toggle={
                       <DropdownToggle
-                        id="pod-toggle-id"
-                        onToggle={toggleIsPodSelectionDropdownOpen}
+                        id="container-type-toggle-id"
+                        onToggle={toggleIsContainerTypeDropdownOpen}
                         toggleIndicator={CaretDownIcon}
                       >
-                        {podSelection}
+                        {containerTypeSelection}
                       </DropdownToggle>
                     }
-                    dropdownItems={podDropDownItems}
+                    dropdownItems={containerTypeDropDownItems}
                   />
-                )}
-              </ToolbarItem>
-              <ToolbarItem>
-                <Dropdown
-                  onSelect={onContainerTypeSelect}
-                  isOpen={isContainerTypeDropdownOpen}
-                  toggle={
-                    <DropdownToggle
-                      id="container-type-toggle-id"
-                      onToggle={toggleIsContainerTypeDropdownOpen}
-                      toggleIndicator={CaretDownIcon}
-                    >
-                      {containerTypeSelection}
-                    </DropdownToggle>
-                  }
-                  dropdownItems={containerTypeDropDownItems}
-                />
-              </ToolbarItem>
-              <ToolbarItem>
-                <LogViewerSearch minSearchChars={3} placeholder="Search value" />
-              </ToolbarItem>
-            </ToolbarContent>
-          </Toolbar>
-        }
-        data={logData}
-        scrollToRow={(logData && logData.length) || 0}
-        height={300}
-        theme="dark"
-      />
-    </>
+                </ToolbarItem>
+                <ToolbarItem>
+                  <LogViewerSearch minSearchChars={3} placeholder="Search value" />
+                </ToolbarItem>
+              </ToolbarContent>
+            </Toolbar>
+          }
+          data={logData}
+          scrollToRow={(logData && logData.length) || 0}
+          height={300}
+          theme="dark"
+        />
+      </Card>
+    </PageSection>
   );
 };
--- pkg/web/src/app/queries/pods.ts
+++ pkg/web/src/app/queries/pods.ts
@@ -1,11 +1,5 @@
-import { useQueryClient, UseQueryResult, UseMutationResult } from 'react-query';
-import {
-  useMockableMutation,
-  getInventoryApiUrl,
-  getClusterApiUrl,
-  useMockableQuery,
-  mockKubeList,
-} from '@app/queries/helpers';
+import { UseQueryResult } from 'react-query';
+import { getClusterApiUrl, useMockableQuery, mockKubeList } from '@app/queries/helpers';
 import {
   authorizedFetch,
   useAuthorizedFetch,
@@ -15,7 +9,6 @@ import {
 import { META } from '@app/common/constants';
 import { IKubeList } from '@app/client/types';
 import { usePollingContext } from '@app/common/context';
-import { podsResource } from '@app/client/helpers';
 import { ContainerType, IPodObject } from '@app/queries/types';
 import { MOCK_LOGS } from '@app/queries/mocks/logs.mock';
 
@@ -29,13 +22,11 @@ export const useClusterPodLogsQuery = (
     {
       enabled: !!podName && !!containerType,
       queryKey: ['pod-logs', podName],
-      // queryFn: useAuthorizedFetch(getClusterApiUrl(`api/v1/namespaces/${META.namespace}/pods/forklift-controller-7db68559b8-tsgh9/log?container=${containerType}`)),
       queryFn: () =>
         new Promise((res, rej) => {
           authorizedFetch<any>(
             getClusterApiUrl(
               `api/v1/namespaces/${META.namespace}/pods/${podName}/log?container=${containerType}`
-              // 'healthz'
             ),
             fetchContext,
             {},
@@ -54,18 +45,15 @@ export const useClusterPodLogsQuery = (
               });
             });
         }),
-      // refetchInterval: usePollingContext().refetchInterval,
       refetchInterval: 1250,
     },
     ''
   );
 };
 
-export const useClusterPodsQuery = (): // containerType: ContainerType | undefined
-UseQueryResult<IKubeList<IPodObject>> => {
+export const useClusterPodsQuery = (): UseQueryResult<IKubeList<IPodObject>> => {
   return useMockableQuery<IKubeList<IPodObject>>(
     {
-      // enabled: !!containerType,
       queryKey: 'cluster-pods-list',
       queryFn: useAuthorizedFetch(getClusterApiUrl(`api/v1/namespaces/${META.namespace}/pods`)),
       refetchInterval: usePollingContext().refetchInterval,
@@ -73,51 +61,3 @@ UseQueryResult<IKubeList<IPodObject>> => {
     mockKubeList([], 'Pods')
   );
 };
-
-// export const usePodLogsQuery = (
-//   containerType: ContainerType,
-//   onSuccess?: (data: any) => void,
-//   onError?: (error: unknown) => void
-//   // ns: string,
-//   // podName: string
-// ) => {
-//   const fetchContext = useFetchContext();
-
-//   const logsQuery = useMockableMutation<any>(
-//     async (options) => {
-//       return new Promise((res, rej) => {
-//         authorizedFetch<any>(
-//           getClusterApiUrl(
-//             `api/v1/namespaces/${META.namespace}/pods/forklift-controller-7db68559b8-tsgh9/log?container=${containerType}`
-//             // 'healthz'
-//           ),
-//           fetchContext,
-//           { 'Content-Type': 'text/plain; charset=utf-8' },
-//           'get',
-//           'text/plain',
-//           true,
-//           options
-//         )
-//           .then((logData) => {
-//             res(logData);
-//           })
-//           .catch((error) => {
-//             rej({
-//               result: 'error',
-//               error: error,
-//             });
-//           });
-//       });
-//     },
-//     {
-//       onSuccess: (data) => {
-//         onSuccess && onSuccess(data);
-//       },
-//       onError: (error) => {
-//         onError && onError(error);
-//       },
-//     }
-//   );
-
-//   return logsQuery;
-// };
