6b404a73f52107e2161c8f5d46636b6bd75fa340
Gilles Dubreuil <gilles@redhat.com>
2022-01-10 16:05:47 +0100

    Bug 1897276: Vsphere provider creation/edition certificate validation (#845)
    * Hook certificate query, display, and validation
    
    * Use default ValidatedTextInput; Use less optional chaining
    
    * Disable button when refocus
    
    * Using fields.hostname directly for query
    
    * Add tls certificates mock
    
    * Adjust tests
    
    * Remove waitFor() for valid input
    
    * Start the hostname field as touched when editing to prevent weird enable/disable state on the Verify button
    
    * Name certificateQuery variable for readability
    
    * Add warning when editing provider if fingerprint doesn't match
    
    * Scroll verify button into view when focusing lower fields
    
    * Fix bug with checking existing fingerprint
    
    * mock scrollIntoView
    
    * Wrap mock with beforeAll()
    
    * use scrllIntoView with useRef()
    
    Co-authored-by: Mike Turley <mike.turley@alum.cs.umass.edu>

--- pkg/web/src/app/Providers/components/AddEditProviderModal/__tests__/AddEditProviderModal.test.tsx
+++ pkg/web/src/app/Providers/components/AddEditProviderModal/__tests__/AddEditProviderModal.test.tsx
@@ -11,6 +11,8 @@ import { NetworkContextProvider } from '@app/common/context';
 import { AddEditProviderModal } from '../AddEditProviderModal';
 import { MOCK_CLUSTER_PROVIDERS } from '@app/queries/mocks/providers.mock';
 
+beforeAll(() => (window.HTMLElement.prototype.scrollIntoView = jest.fn()));
+
 describe('<AddEditProviderModal />', () => {
   const toggleModalAndResetEdit = () => {
     return;
@@ -49,25 +51,23 @@ describe('<AddEditProviderModal />', () => {
       </QueryClientProvider>
     );
 
-    const typeButton = await screen.getByLabelText(/provider type/i);
+    const typeButton = await screen.findByLabelText(/provider type/i);
     userEvent.click(typeButton);
     const oVirtButton = await screen.findByRole('option', { name: /ovirt/i, hidden: true });
     userEvent.click(oVirtButton);
     const caCertField = screen.getByLabelText(/^File upload/);
-    await waitFor(() => {
-      const name = screen.getByRole('textbox', { name: /Name/ });
-      const hostname = screen.getByRole('textbox', {
-        name: /oVirt Engine host name or IP address/i,
-      });
-      const username = screen.getByRole('textbox', { name: /oVirt Engine user name/i });
-      const password = screen.getByLabelText(/^oVirt Engine password/);
-
-      userEvent.type(name, 'providername');
-      userEvent.type(hostname, 'host.example.com');
-      userEvent.type(username, 'username');
-      userEvent.type(password, 'password');
-      userEvent.type(caCertField, '-----BEGIN CERTIFICATE-----abc-----END CERTIFICATE-----');
+    const name = screen.getByRole('textbox', { name: /Name/ });
+    const hostname = screen.getByRole('textbox', {
+      name: /oVirt Engine host name or IP address/i,
     });
+    const username = screen.getByRole('textbox', { name: /oVirt Engine user name/i });
+    const password = screen.getByLabelText(/^oVirt Engine password/);
+
+    userEvent.type(name, 'providername');
+    userEvent.type(hostname, 'host.example.com');
+    userEvent.type(username, 'username');
+    userEvent.type(password, 'password');
+    userEvent.type(caCertField, '-----BEGIN CERTIFICATE-----abc-----END CERTIFICATE-----');
 
     const addButton = await screen.findByRole('dialog', { name: /Add provider/ });
     expect(addButton).toBeEnabled();
@@ -88,32 +88,40 @@ describe('<AddEditProviderModal />', () => {
       </QueryClientProvider>
     );
 
-    const typeButton = await screen.getByLabelText(/provider type/i);
+    const typeButton = await screen.findByLabelText(/provider type/i);
     userEvent.click(typeButton);
     const vsphereButton = await screen.findByRole('option', { name: /vmware/i, hidden: true });
     userEvent.click(vsphereButton);
 
-    await waitFor(() => {
-      const name = screen.getByRole('textbox', { name: /Name/ });
-      const hostname = screen.getByRole('textbox', {
-        name: /vCenter host name or IP address/i,
-      });
-      const username = screen.getByRole('textbox', { name: /vCenter user name/i });
-      const password = screen.getByLabelText(/^vCenter password/);
-      const certFingerprint = screen.getByRole('textbox', {
-        name: /vCenter sha-1 fingerprint/i,
-      });
+    const name = screen.getByRole('textbox', { name: /Name/ });
+    const hostname = screen.getByRole('textbox', {
+      name: /vCenter host name or IP address/i,
+    });
+    const username = screen.getByRole('textbox', { name: /vCenter user name/i });
+    const password = screen.getByLabelText(/^vCenter password/);
 
-      userEvent.type(name, 'providername');
-      userEvent.type(hostname, 'host.example.com');
-      userEvent.type(username, 'username');
-      userEvent.type(password, 'password');
-      userEvent.type(
-        certFingerprint,
-        'AA:39:A3:EE:5E:6B:4B:0D:32:55:BF:EF:95:60:18:90:AF:D8:07:09'
-      );
+    userEvent.type(name, 'providername');
+    userEvent.type(hostname, 'host.example.com');
+    userEvent.click(username);
+    userEvent.type(username, 'username');
+    userEvent.type(password, 'password');
+
+    const verifyButton = await screen.findByLabelText(/verify certificate/i);
+    await waitFor(() => {
+      userEvent.click(verifyButton);
     });
 
+    expect(verifyButton).toBeEnabled();
+    expect(
+      screen.getByText('39:5C:6A:2D:36:38:B2:52:2B:21:EA:74:11:59:89:5E:20:D5:D9:A2')
+    ).toBeInTheDocument();
+    const validateCertificationCheckbox = screen.getByRole('checkbox', {
+      name: /validate certificate/i,
+    });
+
+    userEvent.click(validateCertificationCheckbox);
+
+    expect(validateCertificationCheckbox).toBeChecked();
     const addButton = await screen.findByRole('dialog', { name: /Add provider/ });
     expect(addButton).toBeEnabled();
     const cancelButton = await screen.findByRole('button', { name: /Cancel/ });
@@ -131,31 +139,22 @@ describe('<AddEditProviderModal />', () => {
       </QueryClientProvider>
     );
 
-    const typeButton = await screen.getByLabelText(/provider type/i);
+    const typeButton = await screen.findByLabelText(/provider type/i);
     userEvent.click(typeButton);
     const vsphereButton = await screen.findByRole('option', { name: /vmware/i, hidden: true });
     userEvent.click(vsphereButton);
 
-    await waitFor(() => {
-      const name = screen.getByRole('textbox', { name: /Name/ });
-      const hostname = screen.getByRole('textbox', {
-        name: /vCenter host name or IP address/i,
-      });
-      const username = screen.getByRole('textbox', { name: /vCenter user name/i });
-      const password = screen.getByLabelText(/^vCenter password/);
-      const certFingerprint = screen.getByRole('textbox', {
-        name: /vCenter sha-1 fingerprint/i,
-      });
-
-      userEvent.type(name, 'providername');
-      userEvent.type(hostname, 'hostname');
-      userEvent.type(username, 'username');
-      userEvent.type(password, 'password');
-      userEvent.type(
-        certFingerprint,
-        'AA:39:A3:EE:5E:6B:4B:0D:32:55:BF:EF:95:60:18:90:AF:D8:07:09'
-      );
+    const name = screen.getByRole('textbox', { name: /Name/ });
+    const hostname = screen.getByRole('textbox', {
+      name: /vCenter host name or IP address/i,
     });
+    const username = screen.getByRole('textbox', { name: /vCenter user name/i });
+    const password = screen.getByLabelText(/^vCenter password/);
+
+    userEvent.type(name, 'providername');
+    userEvent.type(hostname, 'hostname');
+    userEvent.type(username, 'username');
+    userEvent.type(password, 'password');
 
     const addButton = await screen.findByRole('button', { name: /Add/ });
     expect(addButton).toBeDisabled();
@@ -193,20 +192,18 @@ describe('<AddEditProviderModal />', () => {
       </QueryClientProvider>
     );
 
-    const typeButton = await screen.getByLabelText(/provider type/i);
+    const typeButton = await screen.findByLabelText(/provider type/i);
     userEvent.click(typeButton);
     const openshiftButton = await screen.findByRole('option', { name: /kubevirt/i, hidden: true });
     userEvent.click(openshiftButton);
 
-    await waitFor(() => {
-      const name = screen.getByRole('textbox', { name: /name/i });
-      const url = screen.getByRole('textbox', { name: /url/i });
-      const saToken = screen.getByLabelText(/^Service account token/);
+    const name = screen.getByRole('textbox', { name: /name/i });
+    const url = screen.getByRole('textbox', { name: /url/i });
+    const saToken = screen.getByLabelText(/^Service account token/);
 
-      userEvent.type(name, 'providername');
-      userEvent.type(url, 'http://host.example.com');
-      userEvent.type(saToken, 'saToken');
-    });
+    userEvent.type(name, 'providername');
+    userEvent.type(url, 'http://host.example.com');
+    userEvent.type(saToken, 'saToken');
 
     const addButton = await screen.findByRole('dialog', { name: /Add provider/ });
     expect(addButton).toBeEnabled();
@@ -225,20 +222,18 @@ describe('<AddEditProviderModal />', () => {
       </QueryClientProvider>
     );
 
-    const typeButton = await screen.getByLabelText(/provider type/i);
+    const typeButton = await screen.findByLabelText(/provider type/i);
     userEvent.click(typeButton);
     const openshiftButton = await screen.findByRole('option', { name: /kubevirt/i, hidden: true });
     userEvent.click(openshiftButton);
 
-    await waitFor(() => {
-      const name = screen.getByRole('textbox', { name: /name/i });
-      const url = screen.getByRole('textbox', { name: /url/i });
-      const saToken = screen.getByLabelText(/^Service account token/);
+    const name = screen.getByRole('textbox', { name: /name/i });
+    const url = screen.getByRole('textbox', { name: /url/i });
+    const saToken = screen.getByLabelText(/^Service account token/);
 
-      userEvent.type(name, 'providername');
-      userEvent.type(url, 'host');
-      userEvent.type(saToken, 'saToken');
-    });
+    userEvent.type(name, 'providername');
+    userEvent.type(url, 'host');
+    userEvent.type(saToken, 'saToken');
 
     const addButton = await screen.getByRole('button', { name: /Add/ });
     expect(addButton).toBeDisabled();
